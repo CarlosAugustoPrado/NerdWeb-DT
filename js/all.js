d561e0fc1ab36656d2266c9893a7fd396701009c
Carlos Augusto Prado <carlosaugusto_prado@yahoo.com.br>
2023-06-16 16:53:14 -0300

    termino do hero e início da seção de slides

---
+++ js/all.js
@@ -0,0 +1,1 @@
+"use strict";var slide_products=new Swiper(".slide-services",{slidesPerView:3,spaceBetween:24,navigation:{nextEl:".s-products .services .slide-services .swiper-navigation .button-next",prevEl:".s-products .services .slide-services .swiper-navigation .button-prev"}});
---
+++ js/scripts/main.js
@@ -0,0 +1,10 @@
+var slide_products = new Swiper(".slide-services", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  navigation: {
+    nextEl:
+      ".s-products .services .slide-services .swiper-navigation .button-next",
+    prevEl:
+      ".s-products .services .slide-services .swiper-navigation .button-prev",
+  },
+});
