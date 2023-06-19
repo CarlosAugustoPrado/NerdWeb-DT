becc4e9370a4890cfbcaa78e7fd9b74f26fb3144
CarlosAugustoPrado <devcarlosaugustoprado@gmail.com>
2023-06-18 23:38:00 -0300

    Finalização da versão de desktop

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,4 +1,5 @@
-var slide_products = new Swiper(".slide-services", {
+// Swiper
+var slideServices = new Swiper(".slide-services", {
   slidesPerView: 3,
   spaceBetween: 24,
   navigation: {
@@ -7,4 +8,30 @@ var slide_products = new Swiper(".slide-services", {
     prevEl:
       ".s-products .services .slide-services .swiper-navigation .button-prev",
   },
+});
+
+var slideProducts = new Swiper(".slide-products", {
+  slidesPerView: 10,
+  spaceBetween: 20,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: "true",
+    snapOnRelease: true,
+    dragSize: "auto",
+  },
+});
+
+//Menu modal
+const btnAbrirModal = document.getElementById("js-btn-sites");
+const modalMenu = document.getElementById("js-modal-menu");
+const imgBtnAbrirModal = btnAbrirModal.querySelector("img");
+
+btnAbrirModal.addEventListener("click", () => {
+  if (modalMenu.classList.contains("active")) {
+    imgBtnAbrirModal.style.transform = "rotate(0deg)";
+    modalMenu.classList.remove("active");
+  } else {
+    imgBtnAbrirModal.style.transform = "rotate(180deg)";
+    modalMenu.classList.add("active");
+  }
 });
